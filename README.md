38ebdd356762da7b266857356a39f74054a544ff
Brian Ingles <brianingles@deephaven.io>
2025-04-03 16:20:06 -0500

    Open docs preview links

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Deephaven Chrome
 
-Deephaven Jira Chrome extension
+Deephaven Internal devlopment Chrome extension.
 
 ## Features
 
 - Highlight required fields in Jira tasks
+- Open Deephaven Docs Preview - right-click context menu item to open doc preview links from Github PRs
+
+## Installation
+
+- Clone this repo
+- Load the directory as an unpacked extension in Chrome
---
+++ background.js
@@ -0,0 +1,65 @@
+const OPEN_DH_DOCS_PREVIEW = "openDeephavenDocsPreview";
+
+chrome.contextMenus.create({
+  id: OPEN_DH_DOCS_PREVIEW,
+  title: "Open Deephaven Docs Preview",
+  contexts: ["link"],
+  documentUrlPatterns: ["https://github.com/deephaven/deephaven.io/pull/*"],
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === OPEN_DH_DOCS_PREVIEW && info.linkUrl) {
+    const prNumber = info.pageUrl.match(/pull\/(\d+)/)[1];
+
+    // The context menu apis don't provide the link text, only the link url.
+    // We need the text to build the path to the preview url, so we inject a
+    // script to get the text content of the link.
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        func: getLinkText,
+        args: [info.linkUrl], // Pass linkUrl to the injected function
+      },
+      (results) => {
+        if (results && results[0] && results[0].result) {
+          const linkText = results[0].result;
+          console.log("Link text:", linkText);
+
+          const [product, ...rest] = linkText.split("/");
+          const path = rest.join("/").replace(/\.md$/, "");
+
+          console.log({
+            product,
+            prNumber,
+            path,
+          });
+
+          const targetUrl = `https://preview.deephaven.io/${product}/pr-${prNumber}/${path}/`;
+
+          // Open the new URL in a new tab
+          chrome.tabs.create({ url: targetUrl });
+        } else {
+          console.error("Failed to get link text");
+        }
+      }
+    );
+  }
+});
+
+function getLinkText(linkUrl) {
+  const hash = linkUrl.split("#")[1];
+  console.log("Looking for link with hash:", hash);
+
+  // Get link in the #files section. This should be the link in the header of
+  // the expandable code diffs. The text content contains the path to the file.
+  const labelEl = document.querySelector(`#files a[href="#${hash}"]`);
+
+  // I don't think we need this, since the hash "should" match a link in the #files
+  // container. Leaving uncommented for now for more testing.
+  // If first match is in the sidebar, the target path is in the previous sibling element
+  // if (labelEl.classList.contains("ActionList-content")) {
+  //   labelEl = labelEl.previousElementSibling;
+  // }
+
+  return labelEl?.textContent.trim();
+}
