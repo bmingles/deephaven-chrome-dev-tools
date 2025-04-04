61b2752118e7b70b2b8c6a865865234573348074
Brian Ingles <brianingles@deephaven.io>
2025-04-04 16:11:33 -0500

    Moved init to onInstalled event

--- deephaven-docs/background.js
+++ deephaven-docs/background.js
@@ -1,59 +1,63 @@
 const OPEN_DH_DOCS_PREVIEW = "openDeephavenDocsPreview";
 
-chrome.contextMenus.create({
-  id: OPEN_DH_DOCS_PREVIEW,
-  title: "Open Deephaven Docs Preview",
-  contexts: ["link"],
-  documentUrlPatterns: ["https://github.com/deephaven/deephaven.io/pull/*"],
-});
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Deephaven Docs Preview extension installed");
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === OPEN_DH_DOCS_PREVIEW && info.linkUrl) {
-    const prNumber = info.pageUrl.match(/pull\/(\d+)/)[1];
+  chrome.contextMenus.create({
+    id: OPEN_DH_DOCS_PREVIEW,
+    title: "Open Deephaven Docs Preview",
+    contexts: ["link"],
+    documentUrlPatterns: ["https://github.com/deephaven/deephaven.io/pull/*"],
+  });
 
-    // The context menu apis don't provide the link text, only the link url.
-    // We need the text to build the path to the preview url, so we inject a
-    // script to get the text content of the link.
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tab.id },
-        func: getLinkText,
-        args: [info.linkUrl], // Pass linkUrl to the injected function
-      },
-      (results) => {
-        if (results && results[0] && results[0].result) {
-          const linkText = results[0].result;
-          console.log("Link text:", linkText);
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === OPEN_DH_DOCS_PREVIEW && info.linkUrl) {
+      const prNumber = info.pageUrl.match(/pull\/(\d+)/)[1];
 
-          if (!linkText.endsWith(".md")) {
-            chrome.notifications.create("", {
-              type: "basic",
-              iconUrl: "icons/dh-light-48.png",
-              title: "Unsupported file type",
-              message: "Only Markdown files are supported for preview",
+      // The context menu apis don't provide the link text, only the link url.
+      // We need the text to build the path to the preview url, so we inject a
+      // script to get the text content of the link.
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tab.id },
+          func: getLinkText,
+          args: [info.linkUrl], // Pass linkUrl to the injected function
+        },
+        (results) => {
+          if (results && results[0] && results[0].result) {
+            const linkText = results[0].result;
+            console.log("Link text:", linkText);
+
+            if (!linkText.endsWith(".md")) {
+              chrome.notifications.create("", {
+                type: "basic",
+                iconUrl: "icons/dh-light-48.png",
+                title: "Unsupported file type",
+                message: "Only Markdown files are supported for preview",
+              });
+              return;
+            }
+
+            const [product, ...rest] = linkText.split("/");
+            const path = rest.join("/").replace(/\.md$/, "");
+
+            console.log({
+              product,
+              prNumber,
+              path,
             });
-            return;
+
+            const targetUrl = `https://preview.deephaven.io/${product}/pr-${prNumber}/${path}/`;
+
+            // Open the new URL in a new tab
+            chrome.tabs.create({ url: targetUrl });
+          } else {
+            console.error("Failed to get link text");
           }
-
-          const [product, ...rest] = linkText.split("/");
-          const path = rest.join("/").replace(/\.md$/, "");
-
-          console.log({
-            product,
-            prNumber,
-            path,
-          });
-
-          const targetUrl = `https://preview.deephaven.io/${product}/pr-${prNumber}/${path}/`;
-
-          // Open the new URL in a new tab
-          chrome.tabs.create({ url: targetUrl });
-        } else {
-          console.error("Failed to get link text");
         }
-      }
-    );
-  }
+      );
+    }
+  });
 });
 
 function getLinkText(linkUrl) {
